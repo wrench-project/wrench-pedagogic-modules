ce92c84847ea1d6191463a9a512c99ea91a0809b
ryan <ryanyt@hawaii.edu>
2018-08-30 18:23:42 -1000

    added more comments to visualization and changed code to use updated host names

--- activity_1_getting_started/visualization/app.js
+++ activity_1_getting_started/visualization/app.js
@@ -3,7 +3,7 @@ var express        = require("express"),
     bodyParser     = require("body-parser"),
     methodOverride = require("method-override"),
     au             = require('ansi_up'),
-    {exec}         = require("child_process"),
+    {exec, spawnSync}         = require("child_process"),
     fs             = require('fs'),
     xml2js         = require('xml2js');
 
@@ -13,26 +13,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 
-var ansi_up     = new au.default;
-var xml_parser  = new xml2js.Parser();
-var xml_builder = new xml2js.Builder({xmldec:{ 'version': '1.0'},
+var ansi_up     = new au.default; // WRENCH produces output to the terminal using ansi colors, ansi_up will apply those colors to <span> html elements
+var xml_parser  = new xml2js.Parser(); // used to parse the platform.xml file so that we change change certain parameters (in this case link bandwidth)
+var xml_builder = new xml2js.Builder({xmldec:{ 'version': '1.0'}, // used to convert the javascript xml object into a string so we can write it back to disk before running the simulation
                                       doctype: {'sysID': 'http://simgrid.gforge.inria.fr/simgrid/simgrid.dtd'}
                                     });
 
+// default route that displays the page with no data and only a form to be filled
 app.get("/", function(req, res) {
     res.render("index");
 });
 
+/**
+ * When the user submits simulation parameters, a POST request is sent to this route and
+ * we do the following:
+ *  1. get simulation parameters ready
+ *  2. read in the platform_file and parse it into a javascript object so that we can set the link bandwidth property to whatever the user input
+ *  3. write out a new platform_file as modified_platform_file with the updated link bandwidth
+ *  4. have node create a process that runs the WRENCH simulator (which uses the modified_platform_file)
+ *  5. save the simulation output so that when can send that back to the client to be rendered in the browser
+ *
+ */
 app.post("/run", function(req, res) {
-    var PATH_PREFIX = '/home/wrench/wrench-pedagogic-modules/' +
-        'activity_1_getting_started/';
 
-    var SIMULATOR              = PATH_PREFIX + (req.body.simulator_number == 1 ? 'simulator_remote_storage' : 'simulator_local_storage');
-    var PLATFORM_FILE          = PATH_PREFIX + 'platform_files/platform.xml';
-    var MODIFIED_PLATFORM_FILE = PATH_PREFIX + 'platform_files/modified_platform.xml';
-    var WORKFLOW_FILE          = PATH_PREFIX + 'workflow_files/workflow.dax';
+    const PATH_PREFIX = __dirname.replace('visualization', '');
 
-    var LOGGING = [
+    const EXECUTABLE             = PATH_PREFIX + (req.body.simulator_number == 1 ? 'simulator_remote_storage' : 'simulator_local_storage');
+    const PLATFORM_FILE          = PATH_PREFIX + 'platform_files/platform.xml';
+    const MODIFIED_PLATFORM_FILE = PATH_PREFIX + 'platform_files/modified_platform.xml';
+    const WORKFLOW_FILE          = PATH_PREFIX + 'workflow_files/workflow.dax';
+    const LINK_BANDWIDTH         = req.body.link_bandwidth;
+
+    // additional WRENCH arguments that filter simulation output (We only want simulation output from the WMS in this activity)
+    const LOGGING = [
         '--log=root.thresh:critical',
         '--log=wms.thresh:debug',
         '--log=simple_wms.thresh:debug',
@@ -40,67 +53,56 @@ app.post("/run", function(req, res) {
         '--log="root.fmt:[%d][%h:%t]%e%m%n"'
     ];
 
-    var simulation_parameters = {
-      executable             : SIMULATOR,
-      platform_file          : PLATFORM_FILE,
-      modified_platform_file : MODIFIED_PLATFORM_FILE,
-      workflow_file          : WORKFLOW_FILE,
-      logging                : LOGGING.join(' '),
-      link_bandwidth         : req.body.link_bandwidth,
+    const SIMULATION_ARGS = [MODIFIED_PLATFORM_FILE, WORKFLOW_FILE].concat(LOGGING);
+    const RUN_SIMULATION_COMMAND = [EXECUTABLE].concat(SIMULATION_ARGS).join(' ');
 
-      getRunCommand: function() {
-        return this.executable + ' ' + this.modified_platform_file + ' ' + this.workflow_file + ' ' + this.logging;
-      }
-    };
+    var data = fs.readFileSync(PLATFORM_FILE);
 
-    run_and_send_response(simulation_parameters, res);
-});
+    var parsed_xml;
+    xml_parser.parseString(data, function(err, result) {
+      parsed_xml = result;
+    });
 
-app.listen(3000, function() {
-    console.log("Server is running!");
-});
+    console.log('\nPlatform File Original Contents');
+    console.log('===============================');
+    console.log(xml_builder.buildObject(parsed_xml) + '\n');
 
+    console.log('\nUpdating Link Bandwidth to ' + LINK_BANDWIDTH + ' MBps');
+    console.log('===================================');
+    parsed_xml.platform.zone[0].link[0]['$'].bandwidth = LINK_BANDWIDTH + "MBps";
+    fs.writeFileSync(MODIFIED_PLATFORM_FILE, xml_builder.buildObject(parsed_xml));
+    console.log(xml_builder.buildObject(parsed_xml) + '\n');
 
-function run_and_send_response(simulation_parameters, res) {
-  fs.readFile(simulation_parameters.platform_file, generateReadFileCallback(simulation_parameters, res));
-}
+    console.log('\nRunning Simulation');
+    console.log('===================');
+    console.log('Executing command: ' + RUN_SIMULATION_COMMAND);
+    var simulation_process = spawnSync(EXECUTABLE, SIMULATION_ARGS);
 
-function generateReadFileCallback(simulation_parameters, res) {
-  return function(err, data) {
-    xml_parser.parseString(data, generateParseStringCallback(simulation_parameters, res));
-  }
-}
+    if (simulation_process.status != 0) {
+      console.log('Something went wrong with the simulation. Possibly check arguments.');
+      console.log(simulation_process.stderr.toString());
+    } else {
+      var simulation_output = simulation_process.stderr.toString();
+      console.log(simulation_output);
 
-function generateParseStringCallback(simulation_parameters, res) {
-  return function(err, result) {
-
-    console.log(xml_builder.buildObject(result));
-    console.log(result.platform.zone[0].link[0]['$'].bandwidth);
-
-    result.platform.zone[0].link[0]['$'].bandwidth = simulation_parameters.link_bandwidth + "MBps";
-    console.log(xml_builder.buildObject(result));
-
-    fs.writeFileSync(simulation_parameters.modified_platform_file, xml_builder.buildObject(result));
-
-    var cmd = simulation_parameters.getRunCommand();
-
-    console.log("Executing cmd: " + cmd);
-    exec(cmd, function(err, stdout, stderr) {
-      if (err) {
-          console.error('We encountered a problem');
-          console.log(stderr);
-          return;
-      }
-
-      console.log(stderr);
-
+      /**
+       * The simulation output uses ansi colors and we want these colors to show up in the browser as well.
+       * Ansi up will take each line, make it into a <span> element, and edit the style so that the text color
+       * is whatever the ansi color was. Then the regular expression just adds in <br> elements so that
+       * each line of output renders on a separate line in the browser.
+       *
+       * The simulation output and the workflowtask data are sent back to the client (see public/scripts.js)
+       */
       var find = '</span>';
       var re = new RegExp(find, 'g');
 
       res.json({
-          "simulation_output": ansi_up.ansi_to_html(stderr).replace(re, '<br>' + find),
-          "task_data": JSON.parse(fs.readFileSync("/home/wrench/workflow_data.json"))
+          "simulation_output": ansi_up.ansi_to_html(simulation_output).replace(re, '<br>' + find),
+          "task_data": JSON.parse(fs.readFileSync(__dirname + '/workflow_data.json'))
       });
-    });
-  }
-}
+    }
+});
+
+app.listen(3000, function() {
+    console.log("Visualization server is running!");
+});
--- activity_1_getting_started/visualization/public/scripts.js
+++ activity_1_getting_started/visualization/public/scripts.js
@@ -1,20 +1,40 @@
 $(function() {
 
     $('#simulator-form').on('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // we don't want the page reloading, so things look dynamic (this will be nice when we use d3's transitions)
 
         var selected_simulator_number = $('#simulator-form input:radio:checked').val();
         var selected_link_bandwidth   = $('#link-bandwidth').val();
 
+        // empty these DOM elements so that new things can be added in
         var simulation_output     = $('#simulation-output').empty();
         var simulation_parameters = $('#simulation-parameters').empty();
 
         var task_details_table      = $('#task-details-table');
+
+        // empty the table body since we will add new simulation data in
         var task_details_table_body = $('#task-details-table > tbody').empty();
 
         $('#chart').css('display', 'block');
-        $('svg').remove();
+        $('svg').remove(); // remove the graph, since we will append a new one to the chart
 
+        /**
+         * In the current implementation, the gantt chart is removed and then created
+         * as a completely new element with the new data. An ajax request is used, however
+         * because it would be nice to take advantage of d3's transition api which will
+         * show nice smooth transition of the data changing on the graph. So instead of a
+         * function call like generate_graph, it would be great to have something like
+         * initialize_graph(empty_data) called when the user first sees the page, and then
+         * update_graph(new_data) when the user clicks on run simulation.
+         *
+         * In summary, we first send the simulation parameters selected by the user to node
+         * (which is running app.js). After node runs the simulation with those parameters,
+         * it will send this client back a response with data from that simulation.
+         *
+         * Upon successfully receiving that data, we first fill up the 'Workflow Data'
+         * table. Then, we do some cleaning of the data (see parse_for_graph(data))
+         * and finally generate the graph with that data.
+         */
         $.ajax({
             url: '/run',
             method: 'POST',
@@ -23,14 +43,15 @@ $(function() {
                                   link_bandwidth: selected_link_bandwidth}),
             success: function(response) {
 
+                // show the users what parameters they used for the simulation
                 simulation_parameters.text(
                   'Platform Link Bandwidth: ' + selected_link_bandwidth + ' MBps | '
                   + 'Storage Service Configuration: '
-                  + (selected_simulator_number == 1 ? 'Storage Service on Fafard' : 'Storage Service on Fafard and Jupiter')
+                  + (selected_simulator_number == 1 ? 'Storage Service on storage_db.edu' : 'Storage Service on storage_db.edu and hpc.edu')
                 );
                 simulation_output.append(response.simulation_output);
 
-                /////////////// generate data table
+                // generate the Workflow Data table
                 task_details_table.css('display', 'block');
                 response.task_data.forEach(function(task, i) {
 
@@ -67,16 +88,29 @@ $(function() {
                     );
                 });
 
+                // get the data ready to be used with D3's stacked layout
                 prepare_for_graph(response.task_data);
-                console.log(response.task_data);
+                /*console.log(response.task_data);*/
+
+                // generate the graph
                 generate_graph(response.task_data);
             }
         });
     });
 });
 
+/**
+ * Format a number to Five decimal places. At the moment, 5 seems like a good number.
+ * Anything less will make some start times look like 0.00 when it's something
+ * like 0.00004
+ */
 var toFiveDecimalPlaces = d3.format('.5f');
 
+/**
+ * When we receive the data as a json array, the keys mentioned in the function
+ * each have a start and end time value. D3's stack layout needs the difference
+ * between these two values so we take care of that here.
+ */
 function prepare_for_graph(data) {
     var keys = ['read', 'compute', 'write', 'whole_task'];
 
@@ -88,6 +122,11 @@ function prepare_for_graph(data) {
     });
 }
 
+/**
+ * Helper function used to get the position of the mouse within the browser window
+ * so that we can have nice moving tooltips. The input is the DOM element we are
+ * interested in (in this case the #chart element).
+ */
 function getOffset(el) {
   const rect = el.getBoundingClientRect();
   return {
@@ -98,8 +137,8 @@ function getOffset(el) {
 
 function generate_graph(data) {
     // Chart params
-    var chart_height = 400;
-    var chart_width  = 800;
+    var chart_height = 400; // this should match the CSS file (later we can change it so that d3 dynamically grabs the value)
+    var chart_width  = 800; // this should match the CSS file (later we can change it so that d3 dynamically grabs the value)
     var svg_height   = 400;
     var padding      = 60;
 
@@ -108,7 +147,7 @@ function generate_graph(data) {
     var write_color   = '#abdcf4';
     var colors        = [read_color, compute_color, write_color];
 
-// Save Reference to the chart div and tooltip div
+    // Save Reference to the chart div and tooltip div
     var chart = document.getElementById('chart');
 
     var tooltip                         = d3.select('#tooltip');
@@ -116,13 +155,13 @@ function generate_graph(data) {
     var tooltip_task_operation          = d3.select('#tooltip-task-operation');
     var tooltip_task_operation_duration = d3.select('#tooltip-task-operation-duration');
 
-// Create the svg element
+    // Create the svg element
     var svg = d3.select('#chart')
         .append('svg')
         .attr('width', chart_width)
         .attr('height', svg_height);
 
-// Create scales
+    // Create scales
     var x_scale = d3.scaleLinear()
         .domain([0, d3.max(data, function (d) {
             return d['read'] + d['compute'] + d['write'] + d['start'];
@@ -139,14 +178,14 @@ function generate_graph(data) {
         .range([chart_height - padding, padding])
         .padding(0.1);
 
-// Stack layout
+    // Stack layout
     var stack = d3.stack().keys([
         'read', 'compute', 'write'
     ]);
 
     var stack_data = stack(data);
 
-// Groups
+    // Groups
     var groups = svg.selectAll('g')
         .data(stack_data)
         .enter()
@@ -158,7 +197,7 @@ function generate_graph(data) {
             return colors[i];
         });
 
-// Add the data
+    // Add the data
     groups.selectAll('rect')
       .data(function(d) {
           return d;
@@ -215,7 +254,7 @@ function generate_graph(data) {
           .attr('stroke', 'none')
       });
 
-// Create axis
+    // Create axis
     var x_axis = d3.axisBottom(x_scale);
     svg.append('g')
         .attr('class', 'x-axis')
