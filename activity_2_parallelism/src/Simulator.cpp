e7cd8345d1d2175733c86cd1fae40a71358f051e
ryan <ryanyt@hawaii.edu>
2018-09-24 15:08:45 -1000

    added some docs

--- activity_2_parallelism/src/Simulator.cpp
+++ activity_2_parallelism/src/Simulator.cpp
@@ -13,39 +13,62 @@
 #include "ActivityScheduler.h"
 #include "./WorkflowToJSON.cpp"
 
-// looks correct based on the visualization
-void generateTaskJoinWorkflow(wrench::Workflow *workflow, int num_tasks_to_join, bool requiresMemory) {
+/**
+ * @brief Generates a join Workflow
+ * @description Creates a workflow where num_tasks_to_join independent tasks are joined into one task. All tasks at the
+ *              top level compute for the same amount of time. The final task computes for a fraction of what the top level
+ *              tasks compute for.
+ * @param workflow
+ * @param num_tasks_to_join: number of tasks to join into one final task
+ * @param file_size: file size for all files in this workflow
+ * @param requires_memory: bool specifying if the tasks have a RAM requirement of 9.0GB or not
+ */
+void generateTaskJoinWorkflow(wrench::Workflow *workflow, int num_tasks_to_join, double file_size, bool requires_memory) {
+
+    if (workflow == nullptr) {
+        throw std::invalid_argument("generateTaskJoinWorkflow(): invalid workflow");
+    }
+
+    if (num_tasks_to_join < 2) {
+        throw std::invalid_argument("generateTaskJoinWorkflow(): number of tasks to join must be at least 2");
+    }
+
+    if (file_size < 1.0) {
+        throw std::invalid_argument("generateTaskJoinWorkflow(): file size must be at least 1.0 bytes");
+    }
 
     // WorkflowTask specifications
-    const double               FLOPS = 60 * 1000.0 * 1000.0 * 1000.0 * 1000.0;
+    const double               FLOPS = 60 * 60 * 1000.0 * 1000.0 * 1000.0 * 1000.0;
     const unsigned long    MIN_CORES = 1;
     const unsigned long    MAX_CORES = 1;
     const double PARALLEL_EFFICIENCY = 1.0;
-    const double  MEMORY_REQUIREMENT = requiresMemory ? 9.0 * 1000.0 * 1000.0 * 1000.0 : 0.0;
-
-    // WorkflowFile size
-    const double FILE_SIZE = 100;
+    const double  MEMORY_REQUIREMENT = requires_memory ? 9.0 * 1000.0 * 1000.0 * 1000.0 : 0.0;
 
     // add final task
-    auto final_task = workflow->addTask("task" + std::to_string(num_tasks_to_join), FLOPS, MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
+    auto final_task = workflow->addTask("task" + std::to_string(num_tasks_to_join), (FLOPS / 12), MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
 
     // create number of desired tasks to join into one
     for (int i = 0; i < num_tasks_to_join; ++i) {
         std::string task_id("task" + std::to_string(i));
         auto current_task = workflow->addTask("task" + std::to_string(i), FLOPS, MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
-        current_task->addInputFile(workflow->addFile(task_id + ".in", FILE_SIZE));
+        current_task->addInputFile(workflow->addFile(task_id + ".in", file_size));
 
-        auto output_file = workflow->addFile(task_id + ".out", FILE_SIZE);
+        auto output_file = workflow->addFile(task_id + ".out", file_size);
         current_task->addOutputFile(output_file);
         final_task->addInputFile(output_file);
         workflow->addControlDependency(current_task, final_task);
     }
 
-    final_task->addOutputFile(workflow->addFile("task" + std::to_string(num_tasks_to_join) + ".out", FILE_SIZE));
+    final_task->addOutputFile(workflow->addFile("task" + std::to_string(num_tasks_to_join) + ".out", file_size));
 }
 
-// cant change host attributes programatically at the moment so will use pugixml to set num_nodes and num_cores for our cluster
-// TODO: instead, just write the string to /tmp
+/**
+ * @brief Generates a platform with two hosts and a cluster
+ * @description Generates a platform where the cluster has a configurable number of nodes and number of cores per node
+ * @param platform_file_path: path to write the platform file to
+ * @param num_nodes: number of nodes in the cluster
+ * @param num_cores: number of cores per node 
+ */
 void generatePlatformWithHPCSpecs(std::string platform_file_path, int num_nodes, int num_cores) {
 
     if (platform_file_path.empty()) {
@@ -66,7 +89,7 @@ void generatePlatformWithHPCSpecs(std::string platform_file_path, int num_nodes,
                       "<platform version=\"4.1\">\n"
                       "   <zone id=\"AS0\" routing=\"Full\">\n"
                       "     <cluster id=\"hpc.edu\" prefix=\"hpc.edu/node_\" suffix=\"\" radical=\"0-";
-            xml += std::to_string(num_nodes)  + "\" core=\"" + std::to_string(num_cores) + "\" speed=\"1000Gf\" bw=\"125GBps\" lat=\"10us\" router_id=\"hpc_gateway\">\n";
+            xml += std::to_string(num_nodes)  + "\" core=\"" + std::to_string(num_cores) + "\" speed=\"1000Gf\" bw=\"10Gbps\" lat=\"10us\" router_id=\"hpc_gateway\">\n";
             xml += "         <prop id=\"ram\" value=\"32000000000\"/>\n";
             xml += "        </cluster>\n";
             xml += "      <zone id=\"AS2\" routing=\"Full\">\n";
@@ -75,7 +98,7 @@ void generatePlatformWithHPCSpecs(std::string platform_file_path, int num_nodes,
             xml += "      <zone id=\"AS3\" routing=\"Full\">\n";
             xml += "          <host id=\"my_lab_computer.edu\" speed=\"1000Gf\" core=\"1\"/>\n";
             xml += "      </zone>\n";
-            xml += "      <link id=\"link1\" bandwidth=\"10MBps\" latency=\"100us\"/>\n";
+            xml += "      <link id=\"link1\" bandwidth=\"1Gbps\" latency=\"100us\"/>\n";
             xml += "      <zoneRoute src=\"AS2\" dst=\"hpc.edu\" gw_src=\"storage_db.edu\" gw_dst=\"hpc_gateway\">\n";
             xml += "        <link_ctn id=\"link1\"/>\n";
             xml += "      </zoneRoute>\n";
@@ -98,50 +121,56 @@ int main(int argc, char** argv) {
     wrench::Simulation simulation;
     simulation.init(&argc, argv);
 
-    char *num_nodes_input     = argv[1];
-    char *num_cores_input     = argv[2];
-    char *num_task_join_input = argv[3];
-    std::string task_memory_required_input  = std::string(argv[4]);
-
     const int MAX_NODES         = 32;
     const int MAX_CORES         = 64;
     const int MAX_TASKS_TO_JOIN = 50;
+    const double MAX_FILE_SIZE  = 1000.0 * 1000.0 * 1000.0 * 1000.0;
 
     int  NUM_HPC_NODES;
     int  NUM_CORES_PER_HPC_NODE;
     int  NUM_TASKS_TO_JOIN;
+    double FILE_SIZE;
     bool TASK_MEMORY_REQUIRED;
 
     try {
 
-        if (argc != 5) {
+        if (argc != 6) {
             throw std::invalid_argument("bad args");
         }
 
-        NUM_HPC_NODES = std::stoi(std::string(num_nodes_input));
+        NUM_HPC_NODES = std::stoi(std::string(argv[1]));
 
         if (NUM_HPC_NODES < 1 || NUM_HPC_NODES > MAX_NODES) {
             std::cerr << "Invalid number of HPC execution nodes. Enter a value in the range [1, " + std::to_string(MAX_NODES) + "]" << std::endl;
             throw std::invalid_argument("invalid number of execution nodes");
         }
 
-        NUM_CORES_PER_HPC_NODE = std::stoi(std::string(num_cores_input));
+        NUM_CORES_PER_HPC_NODE = std::stoi(std::string(argv[2]));
 
         if (NUM_CORES_PER_HPC_NODE < 1 || NUM_CORES_PER_HPC_NODE > MAX_CORES) {
             std::cerr << "Invalid number of cores per HPC node. Enter a value in the range [1, " + std::to_string(MAX_CORES) + "]" << std::endl;
             throw std::invalid_argument("invalid number of cores per hpc node");
         }
 
-        NUM_TASKS_TO_JOIN = std::stoi(std::string(num_task_join_input));
+        NUM_TASKS_TO_JOIN = std::stoi(std::string(argv[3]));
 
         if (NUM_TASKS_TO_JOIN < 2 || NUM_TASKS_TO_JOIN > MAX_TASKS_TO_JOIN) {
             std::cerr << "Invalid number of tasks to join. Enter a value in the range [2, " + std::to_string(MAX_TASKS_TO_JOIN) + "]" << std::endl;
             throw std::invalid_argument("invalid number of tasks to join");
         }
 
-        if (task_memory_required_input == "RAM_REQUIRED") {
+        FILE_SIZE = std::stod(std::string(argv[4]));
+
+        if (FILE_SIZE < 1.0 || FILE_SIZE > MAX_FILE_SIZE) {
+            std::cerr << "Invalid file sizes. Enter a value in the range [1, " + std::to_string(MAX_FILE_SIZE) + "]" << std::endl;
+            throw std::invalid_argument("invalid file size");
+        }
+
+        std::string task_memory_required_arg  = std::string(argv[5]);
+
+        if (task_memory_required_arg == "RAM_REQUIRED") {
             TASK_MEMORY_REQUIRED = true;
-        } else if (task_memory_required_input == "RAM_NOT_REQUIRED") {
+        } else if (task_memory_required_arg == "RAM_NOT_REQUIRED") {
             TASK_MEMORY_REQUIRED = false;
         } else {
             std::cerr << "Invalid memory requirement. Enter RAM_REQUIRED or RAM_NOT_REQUIRED" << std::endl;
@@ -149,17 +178,18 @@ int main(int argc, char** argv) {
         }
 
     } catch(std::invalid_argument &e) {
-        std::cerr << "Usage: activity_2_simulator <platform_file> <num_hpc_nodes> <num_hpc_cores_per_node> <num_tasks_to_join> <task_memory_requirement>" << std::endl;
+        std::cerr << "Usage: activity_2_simulator <platform_file> <num_hpc_nodes> <num_hpc_cores_per_node> <num_tasks_to_join> <file_size> <task_memory_requirement>" << std::endl;
         std::cerr << "   num_hpc_nodes: number of execution nodes in the range [1, " + std::to_string(MAX_NODES) + "]" << std::endl;
         std::cerr << "   num_hpc_cores_per_node: number of cores per node in the range [1, " + std::to_string(MAX_CORES) + "]" << std::endl;
         std::cerr << "   num_tasks_to_join: the number of independent tasks to join in the range [2, " + std::to_string(MAX_TASKS_TO_JOIN) + "]" << std::endl;
+        std::cerr << "   file_size: number of bytes in the range [1, " + std::to_string(MAX_FILE_SIZE) + "]" << std::endl;
         std::cerr << "   task_memory_requirement: RAM_REQUIRED or RAM_NOT_REQUIRED" << std::endl;
         return 1;
     }
 
     // create workflow
     wrench::Workflow workflow;
-    generateTaskJoinWorkflow(&workflow, NUM_TASKS_TO_JOIN, TASK_MEMORY_REQUIRED);
+    generateTaskJoinWorkflow(&workflow, NUM_TASKS_TO_JOIN, FILE_SIZE, TASK_MEMORY_REQUIRED);
 
     // read and instantiate the platform with the desired HPC specifications
     std::string platform_file_path = "/tmp/platform.xml";
@@ -215,6 +245,7 @@ int main(int argc, char** argv) {
     std::cerr << "\ntasks configuration" << std::endl;
     std::cerr << "-------------------" << std::endl;
     std::cerr << std::setw(15) << std::left << "computation: " << some_task->getFlops() << std::endl;
+    std::cerr << std::setw(15) << std::left << "final task computation: " << (some_task->getFlops() / 12) << std::endl;
     std::cerr << std::setw(15) << std::left << "max cores: " << some_task->getMaxNumCores() << std::endl;
     std::cerr << std::setw(15) << std::left << "memory usage: " << some_task->getMemoryRequirement() << std::endl;
 
