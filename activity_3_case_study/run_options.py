592ed42e62a02e135a10afbcc55c30f3c90707a0
ryan <ryanyt@hawaii.edu>
2018-11-18 23:07:44 -1000

    cleaned up run script

--- activity_3_case_study/run_options.py
+++ activity_3_case_study/run_options.py
@@ -1,55 +1,57 @@
 # for testing activity 3 options
 
 from subprocess import Popen, PIPE, STDOUT
-from collections import namedtuple
 
-Platform_Configuration = namedtuple('Platform_Configuration', ['cores', 'ram_in_gb', 'bandwidth_in_mbps'])
+class Activity3_Simulator:
+    run_command = "./activity_simulator {0} {1} {2} {3} --log=root.thresh:critical"
 
-run_command = "./activity_simulator {0} {1} {2} {3} --log=root.thresh:critical"
+    def __init__(self, num_cores, ram_in_gb, bandwidth_in_mbps, input_file_size_in_mb, estimated_execution_time_func):
+        self.num_cores = num_cores
+        self.ram_in_gb = ram_in_gb
+        self.bandwidth_in_mbps = bandwidth_in_mbps
+        self.input_file_size_in_mb = input_file_size_in_mb
+        self.estimated_execution_time_func = estimated_execution_time_func
 
-possible_configurations = {
-    'base'           : Platform_Configuration(2, 16, 100),
-    '2x_cores_10x_bw': Platform_Configuration(4, 16, 1000),
-    '2x_ram_10x_bw'  : Platform_Configuration(2, 32, 1000),
-    '2x_cores_2x_ram': Platform_Configuration(4, 32, 100)
+    def run(self):
+        wrench_process = Popen(Activity3_Simulator.run_command.format(self.num_cores, self.ram_in_gb, self.bandwidth_in_mbps, self.input_file_size_in_mb), shell=True, stdout=PIPE)
+        wrench_process.wait()
+
+        return float(str(wrench_process.communicate()[0], 'utf-8').strip())
+
+    def get_estimated_execution_time(self):
+        return self.estimated_execution_time_func()
+
+    def get_estimated_utilization(self):
+        return 310 / (self.num_cores * self.get_estimated_execution_time())
+
+    def __str__(self):
+        return "Configuration: cores={0}, ram_in_gb={1}, bandwidth_in_mbps={2}, input_file_size_in_mb={3}".format(self.num_cores, self.ram_in_gb, self.bandwidth_in_mbps, self.input_file_size_in_mb)
+
+input_file_size_in_mb = 2000
+
+simulators = {
+    'base'           : Activity3_Simulator(2, 16, 100, input_file_size_in_mb, lambda : (3 * input_file_size_in_mb / 100) + (3 * 100) + 10 + (0.003 / 100)),
+    '2x_cores_10x_bw': Activity3_Simulator(4, 16, 1000, input_file_size_in_mb, lambda : (3 * input_file_size_in_mb / 1000) + (3 * 100) + 10 + (0.003 / 100)),
+    '2x_ram_10x_bw'  : Activity3_Simulator(2, 32, 1000, input_file_size_in_mb, lambda : (3 * input_file_size_in_mb / 1000) + (2 * 100) + 10 + (0.003 / 100)),
+    '2x_cores_2x_ram': Activity3_Simulator(4, 32, 100, input_file_size_in_mb, lambda : (3 * input_file_size_in_mb / 100) + 100 + 10 + (0.003 / 100))
 }
 
-# 2GB input files
-input_file_size_in_mb = 2000
 
 print("---------------------------------------------------------------------")
 
 # run simulation with each option, then print simulated and estimated execution times and utilization
-for configuration_name, configuration in possible_configurations.items():
-    print(configuration_name)
-    print(configuration)
+for simulator_name, simulator in simulators.items():
+    print("Upgrade Option: " + simulator_name)
+    print(str(simulator))
 
     # compute estimated execution time
-    estimated_execution_time = 0
-
-    if configuration_name == 'base':
-        estimated_execution_time = (3 * input_file_size_in_mb / configuration.bandwidth_in_mbps) + (3 * 100) + 10 + (0.003 / 100)
-
-    elif configuration_name == '2x_cores_10x_bw':
-        estimated_execution_time = (3 * input_file_size_in_mb / configuration.bandwidth_in_mbps) + (3 * 100) + 10 + (0.003 / 100)
-
-    elif configuration_name == '2x_ram_10x_bw':
-        estimated_execution_time = (3 * input_file_size_in_mb / configuration.bandwidth_in_mbps) + (2 * 100) + 10 + (0.003 / 100)
-
-    elif configuration_name == '2x_cores_2x_ram':
-        estimated_execution_time = (3 * input_file_size_in_mb / configuration.bandwidth_in_mbps) + 100 + 10 + (0.003 / 100)
-
-    print("estimated_execution_time: {}".format(estimated_execution_time))
+    print("estimated_execution_time: {}".format(simulator.get_estimated_execution_time()))
 
     # execute the simulation
-    wrench_process = Popen(run_command.format(*configuration, input_file_size_in_mb), shell=True, stdout=PIPE)
-    wrench_process.wait()
-
-    simulated_execution_time = float(str(wrench_process.communicate()[0], 'utf-8').strip())
+    simulated_execution_time = simulator.run()
     print("simulated_execution_time: {}".format(simulated_execution_time))
 
-    # compute utilization (3 100TFlop tasks + 1 10TFlop task on 1TFlop core(s) so 310 is always the numerator)
-    utilization = 310 / (configuration.cores * simulated_execution_time)
-    print("utilization: {}".format(utilization))
+    # compute utilization
+    print("estimated_utilization: {}".format(simulator.get_estimated_utilization()))
 
     print("---------------------------------------------------------------------")
