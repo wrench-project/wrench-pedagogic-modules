d9652894474b48aff914f14c46db20f7e282279e
ryan <ryanyt@hawaii.edu>
2019-01-16 20:16:26 -1000

    removed some info

--- visualization/passport-setup.js
+++ visualization/passport-setup.js
@@ -12,8 +12,8 @@ passport.deserializeUser(function(email, done) {
 passport.use(
   new GoogleStrategy({
     callbackURL: "/google/redirect",
-    clientID: "181159033486-i50jrj7cs2vadnophdvi8cump2d3slcn.apps.googleusercontent.com",
-    clientSecret: "1aYmYt0-HeSuMo12N44QAh9D"
+    clientID: ,
+    clientSecret: 
   }, function(accessToken, refreshToken, profile, done) {
     let email = profile['emails'][0]['value'];
     done(null, email);
