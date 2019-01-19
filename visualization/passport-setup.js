217173b620d8cc99f430a20b530ad615a9dda226
ryan <ryanyt@hawaii.edu>
2019-01-18 15:09:17 -1000

    documentation

--- visualization/passport-setup.js
+++ visualization/passport-setup.js
@@ -2,20 +2,27 @@ const passport       = require("passport"),
       GoogleStrategy = require("passport-google-oauth20"),
       keys           = require("./keys.js");
 
+// required function that specifies which user information should
+// be stored in a session, in this case just email
 passport.serializeUser(function(email, done) {
   done(null, email);
 });
 
+// required function that specifies what information will
+// be attached to req.user
 passport.deserializeUser(function(email, done) {
   done(null, email);
 });
 
+// use the google oauth2 strategy to verify identity
 passport.use(
   new GoogleStrategy({
     callbackURL: "/google/redirect",
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret 
+    clientID: keys.google.clientID,        // load from external file
+    clientSecret: keys.google.clientSecret // load from external file
   }, function(accessToken, refreshToken, profile, done) {
+    // of the information sent back by google, we only need
+    // the email (email scope is specified in the route for the callbackURL)
     let email = profile['emails'][0]['value'];
     done(null, email);
   })
