daab085af1cd163215ad6d4aca80eee49070e776
ryan <ryanyt@hawaii.edu>
2019-01-26 15:48:39 -1000

    added some logging and documentation

--- data_server/app.js
+++ data_server/app.js
@@ -9,11 +9,14 @@ app.use(bodyParser.json());
 
 // data posting route
 app.post("/submit_data", function(req, res) {
-  if (req.body.key == keys.dataServer.key) {
-    console.log("received data: ");
-    console.log(req.body.data);
-    console.log("-------------------");
+  let time_now = new Date().toLocaleString("en-US", {timeZone: "Pacific/Honolulu"});
 
+  if (req.body.key == keys.dataServer.key) {
+    console.log(time_now + ": received data");
+    console.log(req.body.data);
+
+    // DATA_FILE will reside on the host machine, and its path will be
+    // mounted into the docker container at /home/data_server/wrench-pedagogic-modules/data_server
     const DATA_FILE = "data_file.json";
     fs.readFile(DATA_FILE, function(err, data) {
         let current_json_data = [];
@@ -22,27 +25,25 @@ app.post("/submit_data", function(req, res) {
         // if the file was able to be read, update the json list with new data
         if (!err) {
             current_json_data = JSON.parse(data);
-            console.log(DATA_FILE + " was read in");
             current_json_data.push(received_data);
-            console.log("its new contents will be ");
-            console.log(current_json_data);
         // if the file was not able to be read, append the new data to the empty json list
         } else {
-            console.log(DATA_FILE + " doesn't exist");
             current_json_data.push(received_data);
         }
 
-        // write the resulting json object to the file (overwritting the old file)
+        // write the resulting json object to the file (overwritting the old file);
+        // cannot simply append since we are writing as json
         fs.writeFile(DATA_FILE, JSON.stringify(current_json_data), function(err) {
             if (err) {
-                console.log("there was a problem writing the json file");
+                console.log("app.post('/submit_data') callback: there was a problem writing the json file");
             }
         });
     });
 
     res.status(201).end();
+
   } else {
-      console.log("shouldn't be here...");
+      console.log(time_now + ": invalid post_request received");
       res.status(400).end();
   }
 });
