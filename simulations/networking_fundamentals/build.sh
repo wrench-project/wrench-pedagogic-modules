d50e6533a5e07fc761bcb1a00a97af0c9a4b8bce
kochwill <wkoch530@gmail.com>
2019-10-13 23:23:07 -1000

    fixed build file

--- simulations/networking_fundamentals/build.sh
+++ simulations/networking_fundamentals/build.sh
@@ -1,2 +1,1 @@
-cmake -DCMAKE_CXX_FLAGS="-DREMOTE_STORAGE" . && make clean && make && mv ./networking_fundamentals_simulator ./networking_fundamentals_simulator \
-    && cmake -DCMAKE_CXX_FLAGS="-DLOCAL_STORAGE" . && make clean && make && mv ./networking_fundamentals_simulator ./networking_fundamentals_simulator
+cmake -DCMAKE_CXX_FLAGS="-DREMOTE_STORAGE" . && make clean && make
