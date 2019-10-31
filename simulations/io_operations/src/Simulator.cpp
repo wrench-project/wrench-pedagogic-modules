5bdbc0f028ea82e67d7a74cf4acbedcbb31b0136
kochwill <wkoch530@gmail.com>
2019-10-31 11:37:29 -1000

    added control dependencies for io sim

--- simulations/io_operations/src/Simulator.cpp
+++ simulations/io_operations/src/Simulator.cpp
@@ -16,12 +16,15 @@
  * @brief Generates an independent-task Workflow
  *
  * @param workflow
- * @param num_tasks: number of tasks
+ * @param task_read: task read size in MB
+ * @param task_write: task write size in MB
+ * @param task_num: number of tasks
  * @param task_gflop: Task GFlop rating
+ * @param io_overlap: Whether IO overlaps w/ computation.
  *
  * @throws std::invalid_argument
  */
-void generateWorkflow(wrench::Workflow *workflow, int task_read, int task_write, int task_gflop, bool io_overlap) {
+void generateWorkflow(wrench::Workflow *workflow, int task_read, int task_write, int task_num, int task_gflop, bool io_overlap) {
 
     if (workflow == nullptr) {
         throw std::invalid_argument("generateWorkflow(): invalid workflow");
@@ -35,12 +38,16 @@ void generateWorkflow(wrench::Workflow *workflow, int task_read, int task_write,
         throw std::invalid_argument("generateWorkflow(): task write must be 0 or above");
     }
 
+    if (task_num <= 0) {
+        throw std::invalid_argument("generateWorkflow(): task num must be greater than 0");
+    }
+
     if (task_gflop < 1) {
         throw std::invalid_argument("generateWorkflow(): task GFlop must be at least 1");
     }
 
     if (io_overlap != true && io_overlap != false){
-        throw std::invalid_argument("generateWorkflow(): io overlap must be 0 or 1");
+        throw std::invalid_argument("generateWorkflow(): io overlap must be a boolean");
     }
 
     // WorkflowTask specifications
@@ -49,29 +56,30 @@ void generateWorkflow(wrench::Workflow *workflow, int task_read, int task_write,
     const unsigned long    MAX_CORES = 1;
     const double PARALLEL_EFFICIENCY = 1.0; //single core so doesn't really matter
     const double MEMORY_REQUIREMENT  = 0.0; //may change this to variable input later?
-    const double           NUM_TASKS = 10.0;
     const double                  GB = 1000.0 * 1000.0 * 1000.0;
     const double                  MB = 1000.0 * 1000.0;
 
 
 
     // create the tasks, need to have computation tasks as well as R/W tasks. If overlaps, IO can run in separate tasks.
-    //TODO add control dependencies
     if (io_overlap == true){
-        for (int i = 0; i < NUM_TASKS; ++i){
-            std::string compute_task_id("compute task #" + std::to_string(i));
-            workflow->addTask(compute_task_id, task_gflop * GFLOP, MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
-
+        for (int i = 0; i < task_num; ++i){
             std::string io_read_task_id("io read task #" + std::to_string(i));
             workflow->addTask(io_read_task_id, 0.0, 0, 0, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
             io_read_task_id->addInputFile(workflow->addFile(io_read_task_id+"::0.in", task_read * MB));
+            workflow->addControlDependency(workflow->getTaskByID(io_read_task_id), workflow->getTaskByID("io write task #" + std::to_string(i-1)))
+
+            std::string compute_task_id("compute task #" + std::to_string(i));
+            workflow->addTask(compute_task_id, task_gflop * GFLOP, MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
+            workflow->addControlDependency(workflow->getTaskByID(compute_task_id), workflow->getTaskByID(io_read_task_id))
 
             std::string io_write_task_id("io write task #" + std::to_string(i));
             workflow->addTask(io_write_task_id, 0.0, 0, 0, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
             io_write_task_id->addOutputFile(workflow->addFile(io_write_task_id+"::0.out", task_write * MB));
+            workflow->addControlDependency(workflow->getTaskByID(io_write_task_id), workflow->getTaskByID(compute_task_id))
         }
     } else {
-        for (int i = 0; i < NUM_TASKS; ++i) {
+        for (int i = 0; i < task_num; ++i) {
             std::string task_id("task #" + std::to_string(i));
             workflow->addTask(task_id, task_gflop * GFLOP, MIN_CORES, MAX_CORES, PARALLEL_EFFICIENCY, MEMORY_REQUIREMENT);
             task_id->addInputFile(workflow->addFile(task_id+"::0.in", task_read * MB));
@@ -130,12 +138,13 @@ int main(int argc, char** argv) {
     const int NUM_CORES         = 1;
     int TASK_READ;
     int TASK_WRITE;
+    int TASK_NUM;
     int TASK_GFLOP;
     bool IO_OVERLAP;
 
     try {
 
-        if (argc != 5) {
+        if (argc != 6) {
             throw std::invalid_argument("bad args");
         }
 
@@ -153,14 +162,21 @@ int main(int argc, char** argv) {
             throw std::invalid_argument("invalid task write amount");
         }
 
-        TASK_GFLOP = std::stoi(std::string(argv[3]));
+        TASK_NUM = std::stoi(std::string(argv[3]));
+
+        if (TASK_NUM<1) {
+            std::cerr << "Invalid task num. Enter a value greater than 0" << std::endl;
+            throw std::invalid_argument("invalid task num amount");
+        }
+
+        TASK_GFLOP = std::stoi(std::string(argv[4]));
 
         if (TASK_GFLOP < 1) {
             std::cerr << "Invalid task gflop. Enter a value greater than 1" << std::endl;
             throw std::invalid_argument("invalid task gflop");
         }
 
-        IO_OVERLAP = std::stoi(std::string(argv[4]));
+        IO_OVERLAP = std::stoi(std::string(argv[5]));
 
         if (IO_OVERLAP != true && IO_OVERLAP != false) {
             std::cerr << "Invalid io overlap. Enter a boolean value" << std::endl;
@@ -172,6 +188,7 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <task_read> <task_write> <task_gflop> <io_overlap>" << std::endl;
         std::cerr << "   task_read: amount read per task in MB [0,9999]" << std::endl;
         std::cerr << "   task_write: amount written per task in MB [0,9999]" << std::endl;
+        std::cerr << "   task_num: number of tasks to compute (>0)" << std::endl;
         std::cerr << "   task_gflop: task GFlop (> 0)" << std::endl;
         std::cerr << "   io_overlap: whether read/write can be done concurrently with computation" << std::endl;
         std::cerr << "" << std::endl;
@@ -181,20 +198,25 @@ int main(int argc, char** argv) {
 
     // create workflow
     wrench::Workflow workflow;
-    generateWorkflow(&workflow, TASK_READ, TASK_WRITE, TASK_GFLOP, IO_OVERLAP);
+    generateWorkflow(&workflow, TASK_READ, TASK_WRITE, TASK_NUM, TASK_GFLOP, IO_OVERLAP);
 
     // read and instantiate the platform with the desired HPC specifications
     std::string platform_file_path = "/tmp/platform.xml";
     generatePlatform(platform_file_path);
+
     simulation.instantiatePlatform(platform_file_path);
 
 
-    const std::string THE_HOST("io_host");
+    const std::string WMS_HOST("io_host");
+    const std::string COMPUTE_HOST("io_host");
+    const std::string STORAGE_HOST("io_host");
+
+    auto io_storage_service = simulation.add(new wrench::SimpleStorageService(STORAGE_HOST, 10000000000000.0));
 
     auto compute_service = simulation.add(
             new wrench::BareMetalComputeService(
-                    THE_HOST,
-                    {{THE_HOST, std::make_tuple(NUM_CORES, wrench::ComputeService::ALL_RAM)}},
+                    COMPUTE_HOST,
+                    {{COMPUTE_HOST, std::make_tuple(NUM_CORES, wrench::ComputeService::ALL_RAM)}},
                     0,
                     {
                             {wrench::BareMetalComputeServiceProperty::THREAD_STARTUP_OVERHEAD, "0"},
@@ -211,6 +233,12 @@ int main(int argc, char** argv) {
     wms->addWorkflow(&workflow);
 
     //TODO add storage
+
+    simulation.add(new wrench::FileRegistryService(WMS_HOST));
+
+    // stage the input files
+    std::map<std::string, wrench::WorkflowFile *> input_files = workflow.getInputFiles();
+    simulation.stageFiles(input_files, io_storage_service);
 
 
     simulation.launch();
